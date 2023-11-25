5b161f78e9901c048fe95cfe9ff9a731e767e628
Viren <viren15152@gmail.com>
2023-11-25 19:37:00 +0000

    Added first 3 questions to my questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,27 @@
-// TODO: Include packages needed for this application
+// This section includes packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('Develop/utils/generateMarkdown')
+const generateMarkdown = require('./Develop/utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title name of this project?',
+
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Can you provide a brief description of your project and what it does?',
+  },
+  {
+    type: 'input',
+    name: 'screenshotPath',
+    message: 'Enter the file path or URL of a screenshot (if any):',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
