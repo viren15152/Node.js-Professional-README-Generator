8cca50eb4c9b34f6707615b4db8c5e007da59f56
Viren <viren15152@gmail.com>
2023-11-25 21:35:46 +0000

    Fixed errors in generateMarkdown.js file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,19 +5,32 @@ function renderLicenseBadge(license) {
     return '';
   }
 
-  //This section will map your license choices to corresponding license URLs 
+  //This section will map your license choices to corresponding badge URLs 
+  const licenseBadgeMap = {
+    MIT: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    Apache: 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+    GPL: 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
+  };
+
+  return `[License Badge](${licenseBadgeMap[license]})`;
+}
+
+// I have created a function that returns the license link
+// If there is no license, the it will return an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  //This section will Map your license choices to corresponding license texts 
   const licenseLinkMap = {
     MIT: 'https://opensource.org/licenses/MIT',
     Apache: 'https://opensource.org/licenses/Apache-2.0',
-    GPL: 'https://www.gnu.orglicenses/gpl-3.0.html',
+    GPL: 'https://www.gnu.org/licenses/gpl-3.0.html',
   };
 
   return `[License Link](${licenseLinkMap[license]})`;
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
