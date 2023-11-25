5092762c570d9a9f16bc40c3de7e525b6a594d80
Viren <viren15152@gmail.com>
2023-11-25 22:03:23 +0000

    I have created a function that returns the license section of and a function to generate the markdown for the README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,14 +32,65 @@ function renderLicenseLink(license) {
   return `[License Link](${licenseLinkMap[license]})`;
 }
 
-// TODO: Create a function that returns the license section of README
+// I have created a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
 
-// TODO: Create a function to generate markdown for README
+  //This section will map your license choices to corresponding license texts 
+  const licenseTextMap = {
+    MIT: 'This project is licensed under the MIT License - see the [MIT License](https://opensource.org/licenses/MIT) for details.',
+    Apache: 'This project is licensed under the Apache License 2.0 - see the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0) for details.',
+    GPL: 'This project is licensed under the GNU General Public License v3.0 - see the [GNU GPL v3.0](https://www.gnu.org/licenses/gpl-3.0.html) for details.',
+  };
+
+  return `## License
+
+${licenseTextMap[license]}`;
+}
+
+// I have created a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license)
+
   return `# ${data.title}
 
+${licenseBadge}
+${licenseLink}
+
+## Description 
+
+${data.description}
+
+## Table of Contents 
+
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+${licenseSection}
+* [Questions](#questions)
+
+## Installation 
+
+${data.installation}
+
+## Usage 
+
+${data.contributing}
+
+${licenseSection}
+
+## Questions
+
+For questions, please contact me via:
+
+- Github: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
