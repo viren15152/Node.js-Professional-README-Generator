4a94866494d36be575cf2d1022b9f9e9390a3047
Viren <viren15152@gmail.com>
2023-11-30 21:35:22 +0000

    Added second README the first is a sample created by my application and the second is one that has been generated manually

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -37,7 +37,7 @@ function renderLicenseLink(license) {
 }
 
 // I have created a function that returns the license section of README.
-// If there is no license, return an empty string.
+// If there is no license, it will return an empty string.
 function renderLicenseSection(license) {
   if (!license) {
     return '';
